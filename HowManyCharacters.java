70e468a43dcd6f9274ba621aae48fd2f6e3efcd1
SteezyShool <77679833+SteezyShool@users.noreply.github.com>
2021-01-19 17:42:07 +0100

    Add files via upload

---
+++ HowManyCharacters.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class HowManyCharacters
+{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);                                  
+        String CharactersAZ = sc.nextLine(); 
+        int length = CharactersAZ.length();
+        System.out.println("Tyle wyszło: " +length);              
+    }
+
+    
+}
